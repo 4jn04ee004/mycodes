b3b0111efd0e1e4ca5a196e4832c34ddfb091a43
4jn04ee004 <kumar.alok4241@gmail.com>
2024-09-17 14:12:42 +0900

    updated len_of_list script

--- List_Ques/len_of_list.py
+++ List_Ques/len_of_list.py
@@ -6,8 +6,32 @@ counter = 0
 for _ in lst:
     counter += 1
 
-print(f"Length of the list : {counter}")
+print(f"Length of the list using naive method: {counter}")
 
 # Method 2
 
-print("Length of the list :", len(lst))
+print("Length of the list using in-build len function:", len(lst))
+
+# Method 3
+
+count = sum([1 for _ in lst])
+print("Length of list using list comprehension:", count)
+
+# Method 4
+
+def recur(lst):
+    if not lst:
+        return 0
+    else:
+        return 1 + recur(lst[:-1])
+
+count = recur(lst)
+print(count)
+
+
+# Method 4
+count = 0
+for i, val in enumerate(lst):
+    print(i, val)
+    count += 1
+print("Total list element count :", count)
