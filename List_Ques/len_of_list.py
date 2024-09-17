09806e35499898173341beb1c46a77b2d6a7bbc7
4jn04ee004 <kumar.alok4241@gmail.com>
2024-09-17 11:58:29 +0900

    adding list ques folder

---
+++ List_Ques/len_of_list.py
@@ -0,0 +1,13 @@
+lst = ["orch", "catalog", "CI", "naming", "ipam"]
+
+# Method 1
+
+counter = 0
+for _ in lst:
+    counter += 1
+
+print(f"Length of the list : {counter}")
+
+# Method 2
+
+print("Length of the list :", len(lst))
